147a31b23616f59ac5d1ec13196c8a98b03f905e
zelttrox <dev.zeltrox@gmail.com>
2024-11-13 16:54:50 +0100

    Changement déplacements Camera

--- Assets/Scripts/Characters/CharacterCamera.cs
+++ Assets/Scripts/Characters/CharacterCamera.cs
@@ -3,16 +3,29 @@ using UnityEngine;
 // This class is used to manage the camera of the character using the mouse and the cinemachine asset
 public class CharacterCamera : MonoBehaviour
 {
+    [Header("References")]
+    public Transform orientation;
+    public Transform player;
+    public Transform playerObj;
+    public Rigidbody rb;
+
+    public float rotationSpeed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
 
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        // rotate orientation
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDir.normalized;
 
         // we use the mouse to move the camera
         float mouseX = Input.GetAxis("Mouse X");
