c3b21b69beb02e00e7ac87b40e79a94b5b2a6f82
zelttrox <dev.zeltrox@gmail.com>
2024-11-18 14:50:53 +0100

    Fix Camera

--- Assets/Scripts/Characters/CharacterCamera.cs
+++ Assets/Scripts/Characters/CharacterCamera.cs
@@ -6,14 +6,11 @@ public class CharacterCamera : MonoBehaviour
     [Header("References")]
     public Transform orientation;
     public Transform player;
-    public Transform playerObj;
-    public Rigidbody rb;
 
     public float rotationSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    void Start() {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
@@ -21,8 +18,8 @@ public class CharacterCamera : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void Update()
-    {
+    private void Update() {
+        
         // rotate orientation
         Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
@@ -34,6 +31,5 @@ public class CharacterCamera : MonoBehaviour
         // we rotate the camera
         transform.Rotate(-mouseY, mouseX, 0.0f);
         
-         
     }
 }
--- Assets/Scripts/Characters/CharacterMovement.cs
+++ Assets/Scripts/Characters/CharacterMovement.cs
@@ -3,22 +3,36 @@ using UnityEngine;
 // This class is used to manage the movement of the character
 public class CharacterMovement : MonoBehaviour {
 
-    public CharacterController controller;
-
     public float playerSpeed = 10;
+
+    public Transform Cam;
+    public CharacterController Controller;
+
+
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
     // Update is called once per frame
     void Update() {
-        
         Movement();
-
     }
 
     void Movement() {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(playerSpeed * Time.deltaTime * move);
+        Vector3 cameraForward = Cam.forward;
+        Vector3 cameraRight = Cam.right;
+
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        Vector3 move = cameraForward * z + cameraRight * x;
+        Controller.Move(playerSpeed * Time.deltaTime * move);
     }
 }
