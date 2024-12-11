34417197a44b0bbdef04dbfa06eb4c05f5ad7cbd
zelttrox <dev.zeltrox@gmail.com>
2024-12-11 17:05:29 +0100

    FindNextTorch func

--- Assets/Scripts/Puzzles/Torch.cs
+++ Assets/Scripts/Puzzles/Torch.cs
@@ -2,31 +2,49 @@ using UnityEngine;
 
 public class Torch : MonoBehaviour {
 
+    [SerializeField] private int ID;
+
     public Transform torchPos;
     public LayerMask playerLayer;
 
     private bool playerNear = false;
+    private lightState currentState = lightState.off;
 
-    void Update() {
-        Scan();
+    private enum lightState {
+        off,
+        ignited,
+        on
     }
 
+    void Update() {
+    }
 
     private void Ignite() {
-
     }
 
     private void LightUp() {
-
     }
 
     private void LightOff() {
-
     }
 
     private void Scan() {
         playerNear = Physics.CheckSphere(torchPos.position, 0.3f, playerLayer);
-        Debug.Log("player is nearby!");
+        if (playerNear) {
+            Debug.Log($"Player is near torch " + ID);
+        }
     }
 
+    private void HandleLighting() {
+    }
+
+    private Torch FindNextTorch() {
+        Torch[] allTorches = FindObjectsByType<Torch>(FindObjectsSortMode.None);
+        foreach (Torch torch in allTorches) {
+            if (torch.ID == this.ID + 1) {
+                return torch;
+            }
+        }
+        return null;
+    }
 }
