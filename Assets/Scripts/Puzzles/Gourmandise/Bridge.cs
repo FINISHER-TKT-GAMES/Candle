67896cd57628d12c2759eb856d09356fea0779a1
zelttrox <dev.zeltrox@gmail.com>
2024-12-30 00:27:35 +0100

    Commentaires & modifs Bridge.cs

--- Assets/Scripts/Puzzles/Gourmandise/Bridge.cs
+++ Assets/Scripts/Puzzles/Gourmandise/Bridge.cs
@@ -1,25 +1,23 @@
-using System.Security.Cryptography;
-using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class Bridge : MonoBehaviour {
 
     private bool isBroken = false;
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.tag == "Player" && !isBroken && wck.player.wax >= wck.bridge.weightLimit) {
+    // Détecte le joueur et casse les planches sous ses pieds si son poids est trop elevé
+    private void OnTriggerEnter(Collider collider) {
+        if (collider.tag == "Player" && !isBroken && wck.player.wax >= wck.bridge.weightLimit) {
             BreakPlank();
         }
     }
 
+    // Détruit la planche en lui faisant subir la gravité
     private void BreakPlank() {
         isBroken = true;
         Rigidbody rb = GetComponent<Rigidbody>();
-        if (rb != null)
-        {
+        if (rb != null) {
             rb.isKinematic = false; // Permet à la planche de tomber
         }
-        // Ajoute un effet visuel ou sonore ici
         Destroy(gameObject, 2f); // Supprime la planche après 2 secondes
     }
 }
