38b8973d2faf2e246ee806c7ca904c0106978116
Léo C <leo.collowald@ynov.com>
2025-01-08 09:35:08 +0100

    Transition

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System;
+using System.Collections;
+
 public class MainMenu : MonoBehaviour {
 
     public Animator cameraAnimator;
 
     public void PlayGame()
-    {
-        cameraAnimator.SetTrigger("NewGame");
+{
+    cameraAnimator.SetTrigger("NewGame"); 
+    StartCoroutine(LoadSceneAfterAnimation());
+}
 
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+private IEnumerator LoadSceneAfterAnimation()
+{
+   
+    yield return new WaitForSeconds(cameraAnimator.GetCurrentAnimatorStateInfo(0).length);
+    
+    yield return new WaitForSeconds(2f);
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+}
+
 
     public void QuitGame()
     {
