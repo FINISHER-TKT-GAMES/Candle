413be4c2e60a0f09da2ea5aa9efdd3d3f1a37c98
zelttrox <dev.zeltrox@gmail.com>
2025-03-09 14:57:10 +0100

    Repoussement du joueur par le vent (WIP)

--- Assets/Scripts/World/Wind.cs
+++ Assets/Scripts/World/Wind.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 public class Wind : MonoBehaviour {
 
     public PlayerManager player;
+    public Transform playerObject;
+    public Rigidbody playerRb;
     public Light flameLight;
 
     public float windForce;
 
-    public IEnumerator BlowFlame() {
+    // Réduit l'intensité de la flamme jusqu'à l'éteindre
+    private IEnumerator BlowFlame() {
         float time = 0;
         while (time < player.data.windResistance) {
             yield return new WaitForSeconds(1);
@@ -16,24 +19,31 @@ public class Wind : MonoBehaviour {
             flameLight.intensity -= 0.3f;
             // flameLight.intensity -= player.data.maxIntensity / player.data.windResistance;
         }
-        Debug.Log("Flame's out");
         flameLight.intensity = 0;
+    }
+
+    // Trouve le vecteur [Origine vent - Joueur]
+    private Vector3 GetVector() {
+        return playerObject.transform.position - transform.position;
+    }
+
+    // Repousse le joueur
+    private void PushPlayer() {
+        playerRb.AddForce(GetVector() * windForce, ForceMode.Impulse);
     }
 
     private void OnTriggerEnter(Collider collider) {
         if (collider.CompareTag("Player")) {
-            Debug.Log("Le joueur est détecté dans la zone de vent.");
             StartCoroutine(BlowFlame());
         }
     }
 
-    // private void OnTriggerStay(Collider collider) {
-        
-    // }
+    private void OnTriggerStay(Collider collider) {
+        PushPlayer();
+    }
 
     private void OnTriggerExit(Collider collider) {
         if (collider.CompareTag("Player")) {
-            Debug.Log("Le joueur a quitté la zone de vent.");
         }
     }
 }
